b8f8641e98c6d91170d905cab15dc1285c05d6b2
Anthony Qi <anthony211407@gmail.com>
2025-09-03 09:33:40 +0900

    first add

---
+++ src/test/java/lab/kind/bee/BeeApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.kind.bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
